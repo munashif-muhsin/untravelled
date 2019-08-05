6002cef76b85a26e5645d4ca76fc9ea1469dc1f0
Muhsin <munashif.muhsin@gmail.com>
2019-08-05 11:28:40 +0300

    added sign up modal

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -9,6 +9,10 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+  double _loginOpacity = 1;
+  double _signUpHeight = 140;
+  bool _isSignUpFormVisible = false;
+
   Widget _buildEmailField() {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 20.0),
@@ -121,6 +125,31 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
     );
   }
 
+  Widget _buildSignUpBtn() {
+    return FlatButton(
+      padding: EdgeInsets.all(0),
+      onPressed: () {},
+      child: Container(
+        alignment: Alignment.center,
+        height: 60,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Color(0xFFB79B7E),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          'SIGN UP',
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.white,
+            letterSpacing: 0.9,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget _buildORrow() {
     return Row(
       children: <Widget>[
@@ -182,12 +211,16 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
 
   Widget _buildAnimatedSignUpView() {
     return AnimatedContainer(
-      duration: Duration(milliseconds: 200),
-      height: 140,
+      duration: Duration(milliseconds: 500),
+      height: _signUpHeight,
+      width: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(35), topRight: Radius.circular(35))),
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(35),
+          topRight: Radius.circular(35),
+        ),
+      ),
       child: Container(
         padding: EdgeInsets.only(top: 30.0, left: 20, right: 20),
         child: Column(
@@ -200,26 +233,193 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
                   fontSize: 14,
                   fontWeight: FontWeight.w700),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  'Sign up',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 34),
-                ),
-                IconButton(
-                  icon: Icon(
-                    FontAwesomeIcons.arrowCircleRight,
-                    color: Color(0xFFB79B7E),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _loginOpacity = 0;
+                  _signUpHeight = MediaQuery.of(context).size.height - 130;
+                  Future.delayed(Duration(milliseconds: 200)).then((_) {
+                    setState(() {
+                      _isSignUpFormVisible = true;
+                    });
+                  });
+                });
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Sign up',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 34),
                   ),
-                  onPressed: () {},
-                )
-              ],
-            )
+                  _signUpHeight == 140
+                      ? IconButton(
+                          icon: Icon(
+                            FontAwesomeIcons.arrowCircleRight,
+                            color: Color(0xFFB79B7E),
+                          ),
+                          onPressed: () {},
+                        )
+                      : Container(),
+                ],
+              ),
+            ),
+            _isSignUpFormVisible
+                ? FadeAndSlideIn(
+                    $this: this,
+                    delay: Duration(milliseconds: 200),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 30),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Color(0xFFB79B7E).withOpacity(0.2),
+                              style: BorderStyle.solid,
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: TextField(
+                              keyboardType: TextInputType.phone,
+                              cursorColor: Color(0xFFB79B7E),
+                              style: TextStyle(color: Color(0xFFB79B7E)),
+                              decoration: InputDecoration(
+                                filled: true,
+                                focusColor: Colors.white,
+                                fillColor: Color(0xFFFBF5EE).withOpacity(0.8),
+                                hintText: 'PHONE NUMBER',
+                                hintStyle: TextStyle(
+                                  color: Color(0xFFB79B7E),
+                                  fontWeight: FontWeight.w700,
+                                  letterSpacing: 0.8,
+                                  fontSize: 12,
+                                ),
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide.none),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              color: Color(0xFFB79B7E).withOpacity(0.3),
+                              margin: EdgeInsets.symmetric(horizontal: 15),
+                              height: 1,
+                              width:
+                                  (MediaQuery.of(context).size.width - 130) / 2,
+                            ),
+                            Container(
+                              width: 30,
+                              alignment: Alignment.center,
+                              child: Text(
+                                'OR',
+                                style: TextStyle(
+                                  color: Color(0xFFB79B7E),
+                                  fontSize: 10,
+                                  letterSpacing: 1,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              color: Color(0xFFB79B7E).withOpacity(0.3),
+                              margin: EdgeInsets.symmetric(horizontal: 15),
+                              height: 1,
+                              width:
+                                  (MediaQuery.of(context).size.width - 130) / 2,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Color(0xFFB79B7E).withOpacity(0.2),
+                              style: BorderStyle.solid,
+                              width: 1.0,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: TextField(
+                              keyboardType: TextInputType.emailAddress,
+                              cursorColor: Color(0xFFB79B7E),
+                              style: TextStyle(color: Color(0xFFB79B7E)),
+                              decoration: InputDecoration(
+                                filled: true,
+                                focusColor: Colors.white,
+                                fillColor: Color(0xFFFBF5EE).withOpacity(0.8),
+                                hintText: 'EMAIL',
+                                hintStyle: TextStyle(
+                                  color: Color(0xFFB79B7E),
+                                  fontWeight: FontWeight.w700,
+                                  letterSpacing: 0.8,
+                                  fontSize: 12,
+                                ),
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide.none),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        _buildSignUpBtn(),
+                      ],
+                    ),
+                  )
+                : Container(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDismissSignupBtn() {
+    return Positioned(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _isSignUpFormVisible = false;
+            _loginOpacity = 1;
+            _signUpHeight = 140;
+          });
+        },
+        child: SafeArea(
+          child: FadeAndSlideIn(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Log In',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 34,
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      FontAwesomeIcons.arrowCircleRight,
+                      color: Color(0xFFB79B7E),
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+            $this: this,
+            delay: Duration(milliseconds: 0),
+          ),
         ),
       ),
     );
@@ -247,65 +447,77 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
                   fit: BoxFit.cover,
                 ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  FadeAndSlideIn(
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Text(
-                        'Log In',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 34,
+              AnimatedOpacity(
+                duration: Duration(milliseconds: 500),
+                opacity: _loginOpacity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    FadeAndSlideIn(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          'Log In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 34,
+                          ),
                         ),
                       ),
+                      $this: this,
+                      delay: Duration(milliseconds: 0),
                     ),
-                    $this: this,
-                    delay: Duration(milliseconds: 0),
-                  ),
-                  FadeAndSlideIn(
-                    $this: this,
-                    child: _buildEmailField(),
-                    delay: Duration(milliseconds: 500),
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  FadeAndSlideIn(
-                    $this: this,
-                    child: _buildPasswordRow(),
-                    delay: Duration(milliseconds: 500),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  FadeAndSlideIn(
-                    $this: this,
-                    child: Column(
-                      children: <Widget>[
-                        _buildLoginBtn(),
-                        SizedBox(height: 20),
-                        _buildORrow(),
-                        SizedBox(height: 20),
-                        _buildFBLoginBtn(),
-                      ],
+                    FadeAndSlideIn(
+                      $this: this,
+                      child: _buildEmailField(),
+                      delay: Duration(milliseconds: 500),
                     ),
-                    delay: Duration(milliseconds: 800),
-                  ),
-                  SizedBox(height: 30),
-                  _buildAnimatedSignUpView(),
-                ],
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    FadeAndSlideIn(
+                      $this: this,
+                      child: _buildPasswordRow(),
+                      delay: Duration(milliseconds: 500),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    FadeAndSlideIn(
+                      $this: this,
+                      child: Column(
+                        children: <Widget>[
+                          _buildLoginBtn(),
+                          SizedBox(height: 20),
+                          _buildORrow(),
+                          SizedBox(height: 20),
+                          _buildFBLoginBtn(),
+                        ],
+                      ),
+                      delay: Duration(milliseconds: 800),
+                    ),
+                    SizedBox(height: 170),
+                  ],
+                ),
               ),
+              Positioned(
+                child: FadeAndSlideIn(
+                  child: _buildAnimatedSignUpView(),
+                  $this: this,
+                  delay: Duration(milliseconds: 1200),
+                ),
+                bottom: 0,
+              ),
+              _signUpHeight == 140 ? Container() : _buildDismissSignupBtn()
             ],
           ),
         ));
   }
 }
 
-class FadeAndSlideIn extends StatelessWidget {
+class FadeAndSlideIn extends StatefulWidget {
   final Widget child;
   final $this;
   final Duration delay;
@@ -313,21 +525,32 @@ class FadeAndSlideIn extends StatelessWidget {
   FadeAndSlideIn({this.child, this.$this, this.delay});
 
   @override
-  Widget build(BuildContext context) {
-    AnimationController _animationController = AnimationController(
-        vsync: $this, duration: Duration(milliseconds: 500));
-    Animation _fadeAnimation =
-        CurvedAnimation(parent: _animationController, curve: Curves.easeOut);
-    Animation _offsetAnimation =
-        Tween<Offset>(begin: Offset(0, 0.5), end: Offset.zero)
-            .animate(_animationController);
-    Future.delayed(delay).then((_) {
-_animationController.forward();
-    });
+  _FadeAndSlideInState createState() => _FadeAndSlideInState();
+}
 
+class _FadeAndSlideInState extends State<FadeAndSlideIn> {
+  Animation _fadeAnimation;
+  Animation _offsetAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    AnimationController _animationController = AnimationController(
+        vsync: widget.$this, duration: Duration(milliseconds: 500));
+    _fadeAnimation =
+        CurvedAnimation(parent: _animationController, curve: Curves.easeOut);
+    _offsetAnimation = Tween<Offset>(begin: Offset(0, 0.5), end: Offset.zero)
+        .animate(_animationController);
+    Future.delayed(widget.delay).then((_) {
+      _animationController.forward();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return SlideTransition(
       position: _offsetAnimation,
-      child: FadeTransition(opacity: _fadeAnimation, child: child),
+      child: FadeTransition(opacity: _fadeAnimation, child: widget.child),
     );
   }
 }
